e76901263b7288a47e9fd65ca04f52f9a21b1302
Marjorie Meziere <mmwebforce20@gmail.com>
2021-01-05 16:40:58 +0100

    Ajouts exemples avec boucles et conditions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,33 +14,17 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        //    Je déclare une variable PHP de type String
-        $author = 'Loïs Lane';
-        $jeanDaniel = 'Jean Daniel';
-        // j'instancie un objet standard PHP sans classe créé par nous nous-même
-        //je n'oublie pas l'import | namespaceresolver -> plugin
-        $article = new stdClass();
-        //J'attribut des propriétés à mon objet
-        $article->title = 'Théorie du complot';
-        $article->intro = 'Fascine depuis des lustres ! On vous dit tout';
-        $article->content = 'Bla bla bla';
-        $michel = new stdClass();
-        $michel->name = 'Michel';
-        $michel->age = 23;
-
-        $outcast = 'JD3000';
-
-        $picture = 'https://img.youtube.com/vi/PWgvGjAhvIw/hqdefault.jpg';
+        $user = new stdClass();
+        $user->isConnected = true;
+        $games = ['Starcraft2' => 8,
+         'BF6' => 128,
+          'Metro Exodus' => 1, ];
 
         // J'envoie tout ça à ma vue pour l'afficher
         return $this->render('home/index.html.twig', [
             'name' => 'Page d\'accueil',
-           'article' => $article,
-           'auteur' => $author,
-           'user' => $michel,
-           'jeanDaniel' => $jeanDaniel,
-           'jd3000' => $outcast,
-           'image' => $picture,
+               'user' => $user,
+               'games' => $games,
                   ]);
     }
 }
