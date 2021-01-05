3ef4b9f7ab3662628c14a6f3749e82fa364be51d
Marjorie Meziere <mmwebforce20@gmail.com>
2021-01-05 15:21:15 +0100

    premiers exemples

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +14,33 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        // Je déclare un "name" qui sera envoyé à la vue
+        //    Je déclare une variable PHP de type String
+        $author = 'Loïs Lane';
+        $jeanDaniel = 'Jean Daniel';
+        // j'instancie un objet standard PHP sans classe créé par nous nous-même
+        //je n'oublie pas l'import | namespaceresolver -> plugin
+        $article = new stdClass();
+        //J'attribut des propriétés à mon objet
+        $article->title = 'Théorie du complot';
+        $article->intro = 'Fascine depuis des lustres ! On vous dit tout';
+        $article->content = 'Bla bla bla';
+        $michel = new stdClass();
+        $michel->name = 'Michel';
+        $michel->age = 23;
+
+        $outcast = 'JD3000';
+
+        $picture = 'https://img.youtube.com/vi/PWgvGjAhvIw/hqdefault.jpg';
+
+        // J'envoie tout ça à ma vue pour l'afficher
         return $this->render('home/index.html.twig', [
             'name' => 'Page d\'accueil',
-        ]);
+           'article' => $article,
+           'auteur' => $author,
+           'user' => $michel,
+           'jeanDaniel' => $jeanDaniel,
+           'jd3000' => $outcast,
+           'image' => $picture,
+                  ]);
     }
 }
