3c3a0b250eb7ba9117aebbe3d35cc572801cf321
Marjorie Meziere <mmwebforce20@gmail.com>
2021-01-08 09:55:22 +0100

    Connexion à la base de données, utilisation des commandes doctrine et entity manager

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Fruit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,25 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+
+    //  Pour ajouter dans la base de données :
+    // Le même produit s'ajoute à chaque chargement de page
+
+    public function index(EntityManagerInterface $manager): Response
     {
-        // J'envoie tout ça à ma vue pour l'afficher
+        $banane = new Fruit();
+        $banane->setNom('banane');
+        $banane->setForme('allongée');
+        $banane->setPoids(200);
+        $banane->setPrix(1.2);
+
+        dump($banane);
+
+        $manager->persist($banane);
+        $manager->flush();
+
         return $this->render('home/index.html.twig', [
+            'fruit' => $banane,
                   ]);
     }
 }
---
+++ src/Entity/Fruit.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FruitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FruitRepository::class)
+ */
+class Fruit
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $forme;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $poids;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getForme(): ?string
+    {
+        return $this->forme;
+    }
+
+    public function setForme(string $forme): self
+    {
+        $this->forme = $forme;
+
+        return $this;
+    }
+
+    public function getPoids(): ?int
+    {
+        return $this->poids;
+    }
+
+    public function setPoids(int $poids): self
+    {
+        $this->poids = $poids;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
